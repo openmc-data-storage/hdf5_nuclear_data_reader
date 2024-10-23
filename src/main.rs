8df337ae5b0bd9356ce5d4a0fcb09ccf42514add
shimwell <mail@jshimwell.com>
2024-10-23 11:19:30 +0100

    all mt numbers returned

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use hdf5::File;
-// TODO consider using ndarray to handle the data
-// use ndarray::ArrayBase;
 use std::collections::HashMap;
 use std::path::Path;
 
-fn read_hdf5(filename: &str, temperature_key: &str, reaction_key:  &str,) -> HashMap<String, Vec<f64>> {
+fn read_hdf5(filename: &str, temperature_key: &str) -> HashMap<String, HashMap<String, Vec<f64>>> {
     // Open the HDF5 file
     let file = File::open(Path::new(filename)).expect("Failed to open HDF5 file");
 
@@ -13,7 +11,7 @@ fn read_hdf5(filename: &str, temperature_key: &str, reaction_key:  &str,) -> Has
     for name in &group_names {
         println!("Group name: {}", name);  // this prints Li6 if the Li6.h5 file is loaded
     }
-    // let names=
+
     // Navigate through groups and read the dataset
     let group_nuclide = file.group(&group_names[0]).expect("Failed to open group Li6");
     
@@ -29,21 +27,25 @@ fn read_hdf5(filename: &str, temperature_key: &str, reaction_key:  &str,) -> Has
     let group_reactions_names: Vec<String> = group_reactions.member_names().expect("Failed to get group names");
     println!("{:?}", group_reactions_names);
 
-    
-    let group_reactions_mt = group_reactions.group(&reaction_key).expect("Failed to open dataset");
-    let group_294k = group_reactions_mt.group(temperature_key).expect("Failed to open group 294K");
-    let dataset_xs = group_294k.dataset("xs").expect("Failed to open dataset xs");
-    
-    println!("{:?}", dataset_xs);
-
     let dataset_energy = group_energy.dataset(&temperature_key).expect("Failed to open dataset");
-    
     let energy_data: Vec<f64> = dataset_energy.read_1d().expect("Failed to read dataset").to_vec();
-    let mt_data: Vec<f64> = dataset_xs.read_1d().expect("Failed to read dataset").to_vec();
 
     let mut result = HashMap::new();
-    result.insert("energy".to_string(), energy_data);
-    result.insert("mt_data".to_string(), mt_data);
+    let mut reactions_map = HashMap::new();
+
+    for reaction_name in group_reactions_names {
+        let group_reactions_mt = group_reactions.group(&reaction_name).expect("Failed to open dataset");
+        let group_294k = group_reactions_mt.group(temperature_key).expect("Failed to open group 294K");
+        let dataset_xs = group_294k.dataset("xs").expect("Failed to open dataset xs");
+        let data: Vec<f64> = dataset_xs.read_1d().expect("Failed to read dataset").to_vec();
+        reactions_map.insert(reaction_name, data);
+    }
+
+    let mut energy_map = HashMap::new();
+    energy_map.insert(temperature_key.to_string(), energy_data);
+
+    result.insert("energy".to_string(), energy_map);
+    result.insert("reactions".to_string(), reactions_map);
 
     result
 }
@@ -52,13 +54,15 @@ fn main() {
     // Define the filename and dataset key
     const FILENAME: &str = "Li6.h5";
     const TEMPERATURE: &str = "294K";
-    const REACTION_MT: &str = "reaction_002";
-
+    const REACTION_MT: &str = "reaction_444";
 
     // Call the read_hdf5 function with the filename and dataset key as arguments
-    let result = read_hdf5(FILENAME, TEMPERATURE, REACTION_MT);
+    let result = read_hdf5(FILENAME, TEMPERATURE);
 
     // Print the energy values
     println!("Energy dataset: {:?}", result.get("energy").unwrap());
-    println!("MT Data: {:?}", result.get("mt_data").unwrap());
-}
+    
+    
+    let reactions = result.get("reactions").unwrap().get(REACTION_MT);
+        println!("MT reaction: {:?}", reactions);
+    }
